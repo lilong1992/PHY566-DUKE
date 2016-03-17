8a3cb39cd6cacdb50071be1f2873853a3aafada3
Wenzhe Yu <wy29@duke.edu>
2016-03-17 15:20:35 -0400

    VY: First draft of problem2 doc

--- src/2_diffusion.py
+++ src/2_diffusion.py
@@ -3,7 +3,7 @@ import pylab as plt
 
 # parameters
 L  = 100.0   # total length to solve
-nx = 1000    # number of length steps
+nx = 2000    # number of length steps
 T  = 1.0     # total time to solve
 nt = 10000   # number of time steps
 D  = 2.0     # diffusion constant
@@ -14,7 +14,7 @@ dt = T/nt
 
 # initialize u
 u = np.zeros((nx+1,nt+1))
-for i in range(int(nx/2-1/dx),int(nx/2+1/dx+1)):  # initial peak width = 2
+for i in range(int(nx/2-0.5/dx),int(nx/2+0.5/dx+1)):  # initial peak width = 1
     u[i,0] = H
 
 # computational kernel: finite difference
@@ -26,22 +26,25 @@ for j in range(nt):         # time controled by j
 # array for plotting
 x = np.linspace(0,L,nx+1)
 
+for i in range(6):
+    print u[nx/2,i*nt/5]
+
 # plot
 plt.figure()
-plt.plot(x,u[:,0],'-r',label='t=0.0')
-plt.plot(x,u[:,nt/5],'-b',label='t=0.2')
-plt.plot(x,u[:,2*nt/5],'-y',label='t=0.4')
-plt.plot(x,u[:,3*nt/5],'-g',label='t=0.6')
-plt.plot(x,u[:,4*nt/5],'-c',label='t=0.8')
-plt.plot(x,u[:,nt],'-m',label='t=1.0')
-plt.legend(fontsize=12,loc='upper right')
+#plt.plot(x,u[:,0],'-r',label='t=0.0')
+plt.plot(x,u[:,nt/5],'-r',label='t=0.2',linewidth=2)
+plt.plot(x,u[:,2*nt/5],'-b',label='t=0.4',linewidth=2)
+plt.plot(x,u[:,3*nt/5],'-y',label='t=0.6',linewidth=2)
+plt.plot(x,u[:,4*nt/5],'-c',label='t=0.8',linewidth=2)
+plt.plot(x,u[:,nt],'-m',label='t=1.0',linewidth=2)
+plt.legend(fontsize=15,loc='upper right')
 plt.xlabel('x',fontsize=20,fontweight='bold')
 plt.ylabel('u',fontsize=20,fontweight='bold')
 plt.xlim(40,60) # only plot the central part
-plt.ylim(0,60)
+plt.ylim(0,28)
 plt.grid()
-plt.xticks((40,50,60),(' ','L/2',' '),fontsize=12)
-plt.yticks((0,12.5,25,37.5,50),('0',' ','H/2',' ','H'),fontsize=12)
+plt.xticks((40,50,60),(' ','0',' '),fontsize=14)
+plt.yticks((0,12.5,25),('0','H/4','H/2'),fontsize=14)
 #plt.title('1D diffusion equation',fontsize=22, fontweight='bold')
-#plt.savefig('group_2b.pdf')
+plt.savefig('2b_1.pdf')
 plt.show()
