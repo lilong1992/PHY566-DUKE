ab9370148c398f78d1a7f25b22e4652902455211
yanshen0 <shen.yan@duke.edu>
2016-03-17 16:03:14 -0400

    I added the Gaussian fitting

--- src/2_diffusion.py
+++ src/2_diffusion.py
@@ -48,3 +48,35 @@ plt.yticks((0,12.5,25),('0','H/4','H/2'),fontsize=14)
 #plt.title('1D diffusion equation',fontsize=22, fontweight='bold')
 plt.savefig('2b_1.pdf')
 plt.show()
+
+## Gaussian fit to find the variance
+from scipy.optimize import curve_fit  #using curve_fit from scipy
+
+def gauss(x, A, sigma):         #define a fitting function
+    return A*np.exp(-(x-L/2)**2/(2.0*sigma**2))
+
+time = np.linspace(0.2,1.0, 5)     # time at 0.2,0.4,0.6,0.8,1.0
+sig = np.zeros(5)                  # to store sigma at different times
+sig0 = np.sqrt(2.0*D*time)         #calculate sigma(t) = sqrt(2*D*t)
+
+for i in range(5):             
+    k = i+1
+    y = u[:,k*nt/5]
+    coeff, var_matrix = curve_fit(gauss,x,y) #fitting, returning an array:coeff[0] = A, coeff[1] =sigma
+    sig[i] = coeff[1]
+
+#plot sigma versus time
+plt.figure()
+plt.plot(time,sig,'-k',label='fitted $\sigma$(t)')
+plt.plot(time,sig0,'-r',label='$\sigma$(t) from diffusion constant')
+plt.legend(fontsize=12,loc='upper left')
+plt.xlabel('time',fontsize=20)
+plt.ylabel('sigma',fontsize=20)
+plt.xlim(0.1,1.1) 
+#plt.ylim(0.0,2.5)
+plt.grid()
+#plt.title('1D diffusion equation_fit',fontsize=22, fontweight='bold')
+#plt.savefig('group_2b_fit.pdf')
+plt.show()
+#the final result is that the fitted sigma is always a bit larger than the calculated one, but they 
+#have the same trend...    
